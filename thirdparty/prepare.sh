b3cc542eb58ed4f2c34cd4a4845bc04f8fdce884
lisanmiao <lisanmiaoanda@126.com>
2016-09-09 19:11:18 +0800

    update prepare.sh

--- thirdparty/prepare.sh
+++ thirdparty/prepare.sh
@@ -5,7 +5,6 @@
 # Distributed under terms of the MIT license.
 #
 
-
 #prepare boost
 wget http://nchc.dl.sourceforge.net/project/boost/boost/1.61.0/boost_1_61_0.tar.gz .
 tar -zxvf boost_1_61_0.tar.gz
